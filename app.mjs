fb19981428927412250d14b73a49792956f2e548
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-10-01 18:36:28 +0300

    comments added

--- app.mjs
+++ app.mjs
@@ -2,9 +2,11 @@ import express from 'express';
 import morgan from 'morgan';
 
 const app = express();
-
+// logs info about requests
 app.use(morgan('tiny'));
+// converts JSON to to JS Object in POST, PUT, PATCH request
 app.use(express.json());
+// converts form data to JS Object in POST, PUT, PATCH request
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res) => {
